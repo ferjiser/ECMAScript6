565ec5e253b43c34e2ad78bf663fddb1e857f3a6
Fernando Jimenez <fernando.jimenezserrano@amaris.com>
2017-07-31 12:35:32 +0200

    Finished chapter

--- dist/SetMaps.js
+++ dist/SetMaps.js
@@ -71,6 +71,8 @@
 "use strict";
 
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 //Un set es una lista de valores que no pueden contener duplicados. Tiene acceso rapido
 //Un map es una colección de claves-valor
 
@@ -85,6 +87,129 @@ function ejemploSet() {
     console.log('size', set.size);
 }
 ejemploSet();
+
+function ejemploSet2() {
+    var set = new Set();
+    set.add(5);
+    set.add('5');
+    set.add(5); //duplicado asi que será ignorado
+    console.log('set', set);
+    console.log('size', set.size);
+}
+ejemploSet2();
+
+//Inicializar Set
+function ejemploSet3() {
+    var set = new Set([1, 2, 3, 4, 4, 4, 4, 4, 7, 8, 1]);
+    console.log('set', set);
+    console.log('size', set.size); //6. Los duplicados se ignoran
+}
+ejemploSet3();
+
+//Comprobar si existe un valor en el set
+
+function ejemploSet4() {
+    var set = new Set([1, 2, 3, 4, 4, 4, 4, 4, 7, 8, 1]);
+    console.log('set', set);
+    console.log('has 4', set.has(4)); //true
+    console.log('has 6', set.has(6)); //false
+}
+ejemploSet4();
+
+//delete borra un registro y clear borra todos
+
+function ejemploSet5() {
+    var set = new Set([1, 2, 3, 4, 4, 4, 4, 4, 7, 8, 1]);
+    console.log('set', set);
+    console.log('has 4', set.has(4)); //true
+    set.delete(4);
+    console.log('has 4', set.has(4)); //false
+    set.clear();
+    console.log('size', set.size); //0
+}
+ejemploSet5();
+
+//forEach.
+
+function ejemploSet6() {
+    var set = new Set([1, 2]);
+    set.forEach(function (value, key, ownerSet) {
+        //value: valor de la posicion en el set, key: mismo valor, ownerSet: set desde el cual el valor es leido
+        console.log(key + ' ' + value);
+        console.log(ownerSet === set);
+    });
+    /* set.forEach(function(value, key, ownerSet){
+         console.log(key+' '+value);
+         console.log(ownerSet === set);
+     }, this)*/ // Se le puede pasar el this si lo necesitara.
+}
+ejemploSet6();
+
+//Convertir un set en array
+
+function ejemploSet7() {
+    var set = new Set([1, 2, 3, 4, 5, 6]),
+        array = [].concat(_toConsumableArray(set));
+    console.log('array', array);
+}
+ejemploSet7();
+
+//MAPS
+
+function ejemploMap1() {
+    var map = new Map();
+    map.set('title', 'Understandings ES6');
+    map.set('year', 2016);
+    console.log('map', map);
+    console.log('title', map.get('title'));
+    console.log('year', map.get('year'));
+    console.log('author', map.get('author')); //undefined
+}
+ejemploMap1();
+
+function ejemploMap2() {
+    var map = new Map();
+    map.set('title', 'Understandings ES6');
+    map.set('year', 2016);
+    console.log('has title', map.has('title')); //true
+    console.log('has author', map.has('author')); //false
+}
+ejemploMap2();
+
+function ejemploMap3() {
+    var map = new Map();
+    map.set('title', 'Understandings ES6');
+    map.set('year', 2016);
+    console.log('has title', map.has('title')); //true
+    map.delete('title');
+    console.log('has title', map.has('title')); //true
+    map.clear();
+    console.log('size', map.size); //0
+}
+ejemploMap3();
+
+//Maps initialization
+function ejemploMap4() {
+    var map = new Map([['name', 'Nicolas'], ['age', 25]]);
+    console.log('has name', map.has('name')); //true
+    console.log('name', map.get('name'));
+    console.log('has age', map.has('age')); //true
+    console.log('age', map.get('age'));
+    console.log('size', map.size);
+}
+ejemploMap4();
+
+//forEach
+
+function ejemploMap5() {
+    var map = new Map([['name', 'Nicolas'], ['age', 25]]);
+    map.forEach(function (value, key, ownerMap) {
+        //value: valor de la posicion en el map, key: valor de la key, ownerMap: map desde el cual el valor es leido
+        console.log(key + ' ' + value); // name Nicolas
+        console.log(ownerMap === map);
+    });
+}
+ejemploMap5();
 
 /***/ })
 
--- src/SetMaps.js
+++ src/SetMaps.js
@@ -12,3 +12,128 @@ function ejemploSet(){
     console.log('size',set.size);
 }
 ejemploSet();
+
+function ejemploSet2(){
+    let set = new Set();
+    set.add(5);
+    set.add('5');
+    set.add(5); //duplicado asi que será ignorado
+    console.log('set',set);
+    console.log('size',set.size);
+}
+ejemploSet2();
+
+//Inicializar Set
+function ejemploSet3(){
+    let set = new Set([1,2,3,4,4,4,4,4,7,8,1]);
+    console.log('set',set);
+    console.log('size',set.size); //6. Los duplicados se ignoran
+}
+ejemploSet3();
+
+//Comprobar si existe un valor en el set
+
+function ejemploSet4(){
+    let set = new Set([1,2,3,4,4,4,4,4,7,8,1]);
+    console.log('set',set);
+    console.log('has 4',set.has(4)); //true
+    console.log('has 6',set.has(6)); //false
+}
+ejemploSet4();
+
+//delete borra un registro y clear borra todos
+
+function ejemploSet5(){
+    let set = new Set([1,2,3,4,4,4,4,4,7,8,1]);
+    console.log('set',set);
+    console.log('has 4',set.has(4)); //true
+    set.delete(4);
+    console.log('has 4',set.has(4)); //false
+    set.clear();
+    console.log('size',set.size); //0
+}
+ejemploSet5();
+
+//forEach.
+
+function ejemploSet6(){
+    let set = new Set([1,2]);
+    set.forEach(function(value, key, ownerSet){ //value: valor de la posicion en el set, key: mismo valor, ownerSet: set desde el cual el valor es leido
+        console.log(key+' '+value);
+        console.log(ownerSet === set);
+    });
+   /* set.forEach(function(value, key, ownerSet){
+        console.log(key+' '+value);
+        console.log(ownerSet === set);
+    }, this)*/ // Se le puede pasar el this si lo necesitara.
+}
+ejemploSet6();
+
+//Convertir un set en array
+
+function ejemploSet7(){
+    let set = new Set([1,2,3,4,5,6]),
+        array = [...set];
+    console.log('array',array);
+}
+ejemploSet7();
+
+//MAPS
+
+function ejemploMap1(){
+   let map = new Map();
+    map.set('title', 'Understandings ES6');
+    map.set('year', 2016);
+    console.log('map', map);
+    console.log('title', map.get('title'));
+    console.log('year', map.get('year'));
+    console.log('author', map.get('author')); //undefined
+}
+ejemploMap1();
+
+function ejemploMap2(){
+    let map = new Map();
+    map.set('title', 'Understandings ES6');
+    map.set('year', 2016);
+    console.log('has title', map.has('title')); //true
+    console.log('has author', map.has('author')); //false
+}
+ejemploMap2();
+
+function ejemploMap3(){
+    let map = new Map();
+    map.set('title', 'Understandings ES6');
+    map.set('year', 2016);
+    console.log('has title', map.has('title')); //true
+    map.delete('title');
+    console.log('has title', map.has('title')); //true
+    map.clear();
+    console.log('size', map.size); //0
+
+}
+ejemploMap3();
+
+//Maps initialization
+function ejemploMap4(){
+    let map = new Map([['name', 'Nicolas'],['age', 25]]);
+    console.log('has name', map.has('name')); //true
+    console.log('name', map.get('name'));
+    console.log('has age', map.has('age')); //true
+    console.log('age', map.get('age'));
+    console.log('size', map.size);
+}
+ejemploMap4();
+
+//forEach
+
+function ejemploMap5(){
+    let map = new Map([['name', 'Nicolas'],['age', 25]]);
+    map.forEach(function(value, key, ownerMap){ //value: valor de la posicion en el map, key: valor de la key, ownerMap: map desde el cual el valor es leido
+        console.log(key+' '+value);  // name Nicolas
+        console.log(ownerMap === map);
+    });
+}
+ejemploMap5();
+
+
+
