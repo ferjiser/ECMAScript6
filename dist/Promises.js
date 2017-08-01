e6835886787a0710314aec3725e240a745037956
Fernando Jimenez <fernando.jimenezserrano@amaris.com>
2017-08-01 13:52:43 +0200

    Finished chapter

--- dist/Promises.js
+++ dist/Promises.js
@@ -69,9 +69,18 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/**
- * Created by Fernando Jimenez on 01/08/2017.
- */
+// Se usan para computaciones diferidas o asíncronas.  deferred
+// 3 ESTADOS: PENDING (inicial) FULFILLED (cumplida ok) REJECTED (rechazada)
+
+//new Promise();
+
+//Promise.resolve(value),
+//Promise.reject(reason),
+//Promise.all(iterable) La promesa se devuelve cuando todas las promesas del argumento iterable han sido resueltas.
+//Promise.race(iterable) Devuelve una promesa que resuelve o rechaza tan pronto como una de las promesas re resuelve o se rechaza.
+
+//Promise.prototype.then(onFulfilled, onRejected)
+//Promise.prototype.catch(onRejected) Gestion del fail.
 
 
 /***/ })
